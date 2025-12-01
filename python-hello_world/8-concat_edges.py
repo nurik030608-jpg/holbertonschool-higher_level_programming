823ac8ad3a7866740e4597c8a14a555cf6f1f390
nurik030608-jpg <nurik030608@gmail.com>
2025-12-01 12:35:59 +0400

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str1 = "Programming is an art"
-str2 = "Python"
-print(str1[14:] + " " + str2 + str1[8:10] + str1[12:14])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[34:64] + ' with ' + str[0:6])
+print(str)
