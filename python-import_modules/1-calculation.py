724decc2191134b7e80a49468a1fa5a56eef6635
nurik030608-jpg <nurik030608@gmail.com>
2025-12-02 14:22:51 +0400

    Update 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-def add(a ,b):
-    return a + b
+from calculator_1 import add, sub, mul, div
 
-def sub(a, b):
-    return a - b
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    return a // b
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
