43d1c9cf5a53fbfc00be546a70b24bbe835e6086
Nuri <nurik030608@gmailc.com>
2025-12-04 10:46:51 +0000

    fwfr

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    list_length = len(my_list)
     if idx < 0:
         return None
     if idx >= list_length:
         return None
-     return my_list[idx]
+    return my_list[idx]
 ~
 ~
---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    list_length = len(my_list)
+    if idx < 0:
+        return my_list
+    if idx >= list_length:
+        return my_list
+    my_list[idx] = element
+    return my_list
