e66b419d93155429f41e8a6e4e824aa1c38f8b28
nurik030608-jpg <nurik030608@gmail.com>
2025-12-04 14:39:15 +0400

    Update 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
-idx = 3
 def element_at(my_list, idx):
     if idx < 0:
         return None
     if idx >= list_length:
         return None
      return my_list[idx]
-~
-~
