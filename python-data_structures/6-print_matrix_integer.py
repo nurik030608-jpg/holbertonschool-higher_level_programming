2ffa689dbe38e4475d109b750ae7f8555971e6fd
nurik030608-jpg <nurik030608@gmail.com>
2025-12-05 16:43:30 +0400

    Update 6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, element in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(element), end="")
+            else:
+                print("{:d}".format(element), end=" ")
+        print()
