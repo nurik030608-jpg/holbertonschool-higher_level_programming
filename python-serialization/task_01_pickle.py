9e90f67c1b9762dce05b15117130f6b0d4751125
nurik030608-jpg <nurik030608@gmail.com>
2025-12-19 14:57:51 +0400

    Update task_01_pickle.py

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Module for serializing and deserializing custom objects using pickle.
+"""
+import pickle
+
+
+class CustomObject:
+    """
+    A custom class representing a person with serialization capabilities.
+    """
+
+    def __init__(self, name: str, age: int, is_student: bool):
+        """Initializes the CustomObject attributes."""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Prints the object's attributes in a formatted manner."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """
+        Serializes the current instance and saves it to a file.
+        
+        Args:
+            filename (str): The name of the file to save the object to.
+        """
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except (OSError, IOError):
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Loads an instance of CustomObject from a file.
+
+        Args:
+            filename (str): The file to load the object from.
+
+        Returns:
+            CustomObject: The loaded object or None if an error occurs.
+        """
+        try:
+            with open(filename, 'rb') as f:
+                obj = pickle.load(f)
+                if isinstance(obj, cls):
+                    return obj
+            return None
+        except (FileNotFoundError, pickle.UnpicklingError, EOFError, AttributeError):
+            return None
