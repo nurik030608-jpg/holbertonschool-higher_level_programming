2769b1e8ecbfeab03a1c64f8f3939bcdfb7eb269
nurik030608-jpg <nurik030608@gmail.com>
2025-12-25 17:11:49 +0400

    Update 9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,1 +1,13 @@
+-- Создание таблицы second_table, если она еще не существует
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
 
+-- Вставка нескольких записей одним запросом
+INSERT INTO second_table (id, name, score) VALUES 
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
