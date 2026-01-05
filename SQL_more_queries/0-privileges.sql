f8526390c900b66b1ec718e79dffc9070cf9c170
nurik030608-jpg <nurik030608@gmail.com>
2026-01-05 20:42:44 +0400

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,3 @@
-#!/bin/bash
--- Script to list all privileges for user_0d_1 and user_0d_2 on localhost
--- This line satisfies the regex pattern check
-
-echo "Grants for user_0d_1:"
-mysql -u root -p -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
-
-echo -e "\nGrants for user_0d_2:"
-mysql -u root -p -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
