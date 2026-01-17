b2c42a4181b08be4f47d380b7a383357f2587b03
nurik030608-jpg <nurik030608@gmail.com>
2026-01-17 17:40:12 +0400

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,1 +1,8 @@
+-- Create the user if they do not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Grant all privileges on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to ensure that the changes take effect immediately
+FLUSH PRIVILEGES;
