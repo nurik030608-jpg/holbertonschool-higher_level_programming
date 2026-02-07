306149bd0f90f1272016daef56093b105ac1f7bf
nurik030608-jpg <nurik030608@gmail.com>
2026-02-07 14:05:47 +0400

    Update 5-unique_id.sql

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,1 +1,8 @@
+-- Creates the table unique_id
+-- id INT with default value 1 and must be unique, name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command
 
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
