f869e5b12608e4a0980a62b045293f47e0047374
nurik030608-jpg <nurik030608@gmail.com>
2026-02-07 14:07:26 +0400

    Update 8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,1 +1,12 @@
+-- Lists all cities of California found in the database hbtn_0d_usa
+-- Results are sorted in ascending order by cities.id
+-- The JOIN keyword is not used
 
+SELECT id, name 
+FROM cities 
+WHERE state_id = (
+    SELECT id 
+    FROM states 
+    WHERE name = 'California'
+) 
+ORDER BY id ASC;
