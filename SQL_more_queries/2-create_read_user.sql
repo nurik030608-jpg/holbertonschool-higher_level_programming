b2791d2cfdfbeda092acfa31e95817b33358bd2f
nurik030608-jpg <nurik030608@gmail.com>
2026-02-07 14:03:59 +0400

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,1 +1,14 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 has only SELECT privilege in the database hbtn_0d_2
 
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it doesn't exist and set the password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege on the specific database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to ensure the changes take effect immediately
+FLUSH PRIVILEGES;
