0fa79d45c153b05193fd3a78fda0450d42bc2f5c
nurik030608-jpg <nurik030608@gmail.com>
2026-02-07 14:06:18 +0400

    Update 6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,1 +1,16 @@
+-- Creates the database hbtn_0d_usa and the table states
+-- id INT: unique, auto generated, not null, primary key
+-- name VARCHAR(256): not null
 
+-- Create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create the table
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
