8dccedc71a86661b857df00142033039199cf587
nurik030608-jpg <nurik030608@gmail.com>
2026-02-07 14:04:39 +0400

    Update 3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,1 +1,8 @@
+-- Creates the table force_name
+-- id INT, name VARCHAR(256) cannot be null
+-- The database name will be passed as an argument of the mysql command
 
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
