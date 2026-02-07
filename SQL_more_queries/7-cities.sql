b1aa880c5acb1684b000f7084d61c8ddf28daea8
nurik030608-jpg <nurik030608@gmail.com>
2026-02-07 14:06:53 +0400

    Update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,1 +1,19 @@
+-- Creates the database hbtn_0d_usa and the table cities
+-- id INT: unique, auto generated, not null, primary key
+-- state_id INT: not null, foreign key referencing states(id)
+-- name VARCHAR(256): not null
 
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create the table cities
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
