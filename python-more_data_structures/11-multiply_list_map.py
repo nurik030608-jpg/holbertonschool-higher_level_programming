c545b9f525cb501592e5bb10a586ccae324ffb1e
nurik030608-jpg <nurik030608@gmail.com>
2026-02-08 15:20:42 +0400

    Update 11-multiply_list_map.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-def best_score(a_dictionary):
-    """
-    Returns a key with the biggest integer value.
-    If no score is found, returns None.
-    """
-    if not a_dictionary:
-        return None
-
-    # max iterates keys; key=a_dictionary.get compares their values
-    return max(a_dictionary, key=a_dictionary.get)
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
