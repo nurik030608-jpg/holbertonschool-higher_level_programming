7eb2962b2de903e4166ae9458a01b7b8fdaa742a
nurik030608-jpg <nurik030608@gmail.com>
2026-02-08 15:21:09 +0400

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,31 @@
 
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    """
+    Converts a Roman numeral to an integer.
+    """
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    for i in range(len(roman_string)):
+        current_val = roman_dict.get(roman_string[i], 0)
+        
+        # Check if the next character exists and is greater than current
+        if i + 1 < len(roman_string) and roman_dict.get(roman_string[i+1], 0) > current_val:
+            total -= current_val
+        else:
+            total += current_val
+            
+    return total
