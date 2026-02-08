31e9959c83b9c5ec5c1c5ee707f8a6eb0627755a
nurik030608-jpg <nurik030608@gmail.com>
2026-02-08 15:19:26 +0400

    Update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
 
+def best_score(a_dictionary):
+    """
+    Returns a key with the biggest integer value.
+    If the dictionary is empty or None, returns None.
+    """
+    if not a_dictionary:
+        return None
+
+    # max() with the dictionary's get method as the key for comparison
+    return max(a_dictionary, key=a_dictionary.get)
