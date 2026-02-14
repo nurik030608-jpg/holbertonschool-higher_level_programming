c16c457030170c3ea8269ea604a1c41f5ccc5d40
nurik030608-jpg <nurik030608@gmail.com>
2026-02-14 15:34:19 +0400

    Create 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    for char in str:
+        # Check if the character is lowercase
+        if ord(char) >= 97 and ord(char) <= 122:
+            # Convert to uppercase by subtracting 32 from its ASCII value
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
